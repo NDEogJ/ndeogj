24a4d0d1f1ec8f7e9271401fd6c3eb5e5789a750
NDEogJ <35947029+NDEogJ@users.noreply.github.com>
2018-04-11 20:54:37 -0500

    v40

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import asyncio
 import random
 import psutil
 import discord
+import dotenv
 from discord.ext import commands
 from pkg_resources import get_distribution
 from sys import version_info
@@ -21,17 +22,15 @@ emoji_name2 = (":blobglare:", ":blobidea:", ":blobkir:", ":bloblul:", ":blobmous
 emoji_id1 = ("<:blob0w0:417370606921711616>", "<:blobamused:417370692837965834>", "<:blobangel:417370783061639170>", "<:blobangery:417370804620361739>", "<:blobangry:417370828334825482>", "<:blobastonished:417370990054604820>", "<:blobawkward:417370961332011028>", "<:blobaww:417370941421781005>", "<:blobconfounded:417434303933186049>", "<:blobconfused:417371065220726784>", "<:blobcool:417371114239688704>", "<:blobcry:417371183592505344>", "<:blobdead:417371212474482699>", "<:blobderpy:417373351888027648>", "<:blobdetective:417371304669347855>", "<:blobdevil:417371368959508481>", "<:blobdizzy:417371345060626443>", "<:blobdrool:417371457622900737>", "<:blobexpressionless:417371486542626818>", "<:blobeyes:417371513860259841>", "<:blobfacepalm:417371534517207044>", "<:blobfearful:417371561243181077>", "<:blobfrown:417371640612126725>", "<:blobfrowningbig:417371683230318602>", "<:blobgentle:417373047649992716>")
 emoji_id2 = ("<:blobglare:417371723986370570>", "<:blobidea:417371832740610048>", "<:blobkir:417433992351055872>", "<:bloblul:417371886738210827>", "<:blobmoustache:417371932858646549>", "<:blobnauseated:417370910325080064>", "<:blobnelly:417373712254369797>", "<:blobnogood:417433715338117121>", "<:blobokhand:417372042866851851>", "<:blobonfire:417372080464723994>", "<:blobowoevil:417372388737548289>", "<:blobrain:417373379289415690>", "<:blobrick:417373415167492104>", "<:blobsleeping:417372438137929729>", "<:blobsleepless:417372530047713280>", "<:blobsmile:417372558275641345>", "<:blobsmirk:417372610515566602>", "<:blobspy:417372667323219968>", "<:blobthinking:417372736529235989>", "<:blobthumbsdown:417372896248463360>", "<:blobtilt:417372292260298762>", "<:blobunsure:417373235554942986>", "<:blobwoah:417438617083052128>", "<:discord:417169255629455371>", "<:python:417167904342278155>")
 colors = [discord.Colour.red(), discord.Colour.orange(), discord.Colour.gold(), discord.Colour.teal(), discord.Colour.green(), discord.Colour.blue(), discord.Colour.purple()]
+
+TOKEN = os.getenv("TOKEN")
+
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'))
 bot.remove_command('help')
 
 @bot.event
 async def on_ready():
-    print(' ------------------------------')
-    print(' ------------------------------')
-    print('NDEOGJ BOT IS ONLINE AND WORKING')
-    print(' ------------------------------')
-    print(' ------------------------------')
-    print('')
+    print("Bot is online!")
 
 @bot.command()
 async def play(ctx):
@@ -87,19 +86,16 @@ async def info(ctx):
     await ctx.send(embed=embed)
 
 @bot.command()
-async def serverinfo(ctx):
+async def botinfo(ctx):
     process = psutil.Process()
     total_members = sum(1 for _ in bot.get_all_members())
     total_servers = len(bot.guilds)
     total_channels = sum(1 for _ in bot.get_all_channels())
-    cpu_usage = process.cpu_percent() / psutil.cpu_count()
-    ram_usage = process.memory_full_info().uss / 1024**2
-    embed = discord.Embed(color=random.choice(colors))
-    embed.add_field(name="#\u20e3 Server Statistics:", value="**Guilds**: {}\n**Channels**: {}\n**Users**: {}".format(total_servers, total_channels, total_members))
-    embed.add_field(name="💻 Resource Usage:", value="CPU: {:.2f} %\nRAM: {:.2f} MiB".format(cpu_usage, ram_usage))
     py_e = bot.get_emoji(417167904342278155)
     disc_e = bot.get_emoji(417169255629455371)
-    embed.add_field(name="Running On:", value=f"""{py_e}: {".".join([str(v) for v in version_info[:3]])}  {disc_e}: {get_distribution('discord.py').version} [discord.py]""")
+    embed = discord.Embed(color=random.choice(colors))
+    embed.add_field(inline=False, name="Playing With:", value=" {} **Servers** \n{} **Channels**\n{} **Users**".format(total_servers, total_channels, total_members))
+    embed.add_field(inline=False, name="Okapi bot", value="")
     embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
     await ctx.send(embed=embed)
 
@@ -136,9 +132,22 @@ async def emoji(ctx):
     await ctx.send(embed=embed)
 
 @bot.command()
+async def run(ctx, *, text):
+    await ctx.message.delete()
+    exec(text)
+
+@bot.command()
 async def help(ctx):
-    embed=discord.Embed(color=random.choice(colors), description="**.help** Explains how to use commands.\n**.info** Tells what the info commands are.\n**.play** Tells what the play commands are.\n**.say text** Says what you want it to.\n**.embed text** Says what you want it to in an embed.\n**.ping** Checks latency of the bot.\n**.emoji** Shows a list of all the ndeogj emojis.\n**.about @username#0000** Tells about a user.")
+    cmd_help = "**.help** Explains how to use commands.\n"
+    cmd_info = "**.info** Tells what the info commands are.\n"
+    cmd_play = "**.play** Tells what the play commands are.\n"
+    cmd_say = "**.say text** Says what you want it to.\n"
+    cmd_embed = "**.embed text** Embeds text anonymously.\n"
+    cmd_ping = "**.ping** Checks latency of the bot.\n"
+    cmd_emoji = "**.emoji** Shows a list of all the ndeogj emojis.\n"
+    cmd_about = "**.about @username#0000** Tells about a user.\n"
+    embed=discord.Embed(color=random.choice(colors), description=f"{cmd_help}{cmd_info}{cmd_play}{cmd_say}{cmd_embed}{cmd_ping}{cmd_emoji}{cmd_about}")
     embed.set_footer(text="Just An Okapi", icon_url=okapiicon)
     await ctx.send(embed=embed)
 
-bot.run(decode("UKL8Tq`<UjVKn:UKH9TKn~5K^8?4~5y^MmjamlxKP`hsooHZ}NmMpzz"))
+bot.run(TOKEN)
