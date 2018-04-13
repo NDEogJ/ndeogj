efd2bd4a0f50821d8bb3699a8290d0f84cf46992
NDEogJ <35947029+NDEogJ@users.noreply.github.com>
2018-04-12 20:47:15 -0500

    v43

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 import psutil
 import discord
-import python-dotenv
+import dotenv
 from discord.ext import commands
 from pkg_resources import get_distribution
 from sys import version_info
