178d78be9a8eba03e896f42c7853866391967536
NDEogJ <35947029+NDEogJ@users.noreply.github.com>
2018-04-13 15:40:06 -0500

    v49

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ import asyncio
 import random
 import psutil
 import discord
-import boto
 from boto.s3.connection import S3Connection
 s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 from discord.ext import commands
